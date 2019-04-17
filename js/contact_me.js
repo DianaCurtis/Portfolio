d98a08c1d201606b39885b26109ab0f1bc8d561c
Diana Curtis <dianacurtis92@gmail.com>
2019-04-16 17:03:59 -0700

    changed the black text on the menue bar for the two files

--- js/contact_me.js
+++ js/contact_me.js
@@ -71,7 +71,7 @@ $(function() {
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $('#success > .alert-danger').append('</div>');     
         },
         complete: function() {
           setTimeout(function() {
